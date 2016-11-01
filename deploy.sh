baa579af6bf7a89e44e792ee0d60f435faf24361
John Nguyen <jnguyen@thefrontiergroup.com.au>
2016-11-01 09:56:16 +0800

    fixes asset url

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,13 @@
+#! /bin/bash
+if [ $# -lt 1 ]
+	then
+		echo 'No commit message given'
+		exit
+fi
+MSG=$1
+git checkout master
+rm -rf public
+hugo
+git add .
+git commit -m "$MSG"
 git push origin master && git subtree push --prefix public origin gh-pages
